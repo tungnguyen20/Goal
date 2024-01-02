2628e056e49f4a9ee0f4e58cdf5f24a53b9b2355
Tung Nguyen <nxtungbkhn@gmail.com>
2024-01-02 23:41:27 +0700

    Initial Commit

---
+++ Goal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Goal
+//
+//  Created by Tung Nguyen on 02/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
